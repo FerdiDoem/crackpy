8d43bded493fc30328d47e5e72783257504623ac
Your Name <ferdinand-doemling@web.de>
2026-06-29 20:48:57 +0200

    refactor: share method run dependencies

--- crackpy/methods/__init__.py
+++ crackpy/methods/__init__.py
@@ -6,11 +6,18 @@ logic stay in their method-local modules.
 """
 
 from crackpy.methods.definition import MethodArtifactDefinition, MethodDefinition
-from crackpy.methods.runtime import MethodRunIdentityPolicy, build_manual_crack_tip_estimate
+from crackpy.methods.runtime import (
+    MethodRunDependency,
+    MethodRunIdentityPolicy,
+    build_manual_crack_tip_estimate,
+    build_method_run_dependencies,
+)
 
 __all__ = [
     "MethodArtifactDefinition",
     "MethodDefinition",
+    "MethodRunDependency",
     "MethodRunIdentityPolicy",
     "build_manual_crack_tip_estimate",
+    "build_method_run_dependencies",
 ]
